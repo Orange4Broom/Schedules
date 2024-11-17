093478d52628876ebf931588ca8fede3cfc430f7
Orange4Broom <orang4broom@gmail.com>
2024-11-17 10:06:52 +0100

    Instaling basic packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+      '@typings': path.resolve(__dirname, 'src/typings/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@elements': path.resolve(__dirname, 'src/components/elements/'),
+      '@blocks': path.resolve(__dirname, 'src/components/blocks/'),
+      '@fbase': path.resolve(__dirname, 'src/firebase/'),
+    },
+  },
+});
