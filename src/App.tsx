1f91fdc37289e682b2d018fed2fdd2b1904c2016
Orange4Broom <orang4broom@gmail.com>
2024-11-17 15:36:17 +0100

    Updating app

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { PdfForm } from '@blocks/pdfForm/PdfForm';
 export const App: FC = () => {
   return (
     <DefaultLayout>
-      <h1>Hello, world!</h1>
       <PdfForm />
     </DefaultLayout>
   );
