9223c2fbb1b47c73190d478dee2a428488d6c93a
Orange4Broom <orang4broom@gmail.com>
2024-11-17 15:36:01 +0100

    Updating pdfform

--- src/components/blocks/pdfForm/PdfForm.tsx
+++ src/components/blocks/pdfForm/PdfForm.tsx
@@ -1,7 +1,60 @@
+import { useState } from 'react';
+import { RadioSection } from '@/components/elements/radioSection/RadioSection';
+import { MockUpData } from '@/data/MockUpData';
+import './pdfForm.scss';
+import { Icon } from '@/components/elements/icon/Icon';
+
 export const PdfForm = () => {
+  const [selectedOptions, setSelectedOptions] = useState<
+    (number | string | boolean)[]
+  >([]);
+
+  console.log(selectedOptions);
+
   return (
-    <form className="form" onSubmit={() => console.log('Bruh')}>
-      <button className="form__submit-button">Vytvořit PDF</button>
+    <form className="form">
+      {MockUpData.sections.map((section) => {
+        if (section.type === 'radio') {
+          return (
+            <RadioSection
+              key={section.name}
+              name={section.name}
+              title={section.title}
+              section={section}
+              setSelectedOptions={setSelectedOptions}
+              selectedOptions={selectedOptions}
+            />
+          );
+        }
+
+        if (section.type === 'number') {
+          return (
+            <input
+              key={section.name}
+              type="number"
+              name={section.name}
+              placeholder={section.title}
+              className="form__input"
+            />
+          );
+        }
+
+        if (section.type === 'text') {
+          return (
+            <textarea
+              key={section.name}
+              name={section.name}
+              placeholder={section.title}
+              className="form__textarea"
+            />
+          );
+        }
+
+        return null;
+      })}
+      <button className="form__submit-button">
+        Vytvořit PDF <Icon name="file" type="fas" />
+      </button>
     </form>
   );
 };
